14885253fa60a8f48a0ccf09e39c2a29e847f469
Pedro de Lucas Soares Cardoso <pedro.soares@academico.ifrn.edu.br>
2019-07-01 08:51:37 -0300

    new migration

--- seeds/tasks.js
+++ seeds/tasks.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
   return knex('tasks').del()
     .then(function () {
       return knex('tasks').insert([
-        {title: 'Bom dia', description: 'Você deu bom dia para alguém há 44 dias'},
         {title: 'Ligação', description: 'Você ligou para seus amigos há 44 dias'},
-        {title: 'Zap', description: 'Envie um zap aos seus amigos em 6 horas'},
+        {title: 'Zap', description: 'no zap zap zap zap'},
+        {title: 'Bom dia', description: 'Mandou a true pro clan'}
       ]);
     });
 };
